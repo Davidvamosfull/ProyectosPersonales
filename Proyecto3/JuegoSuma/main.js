d52d6a9051202c46888bd332fbacb7a028b6e318
Davidvamosfull <jieli3837@gmail.com>
2024-07-15 20:14:26 +0200

    Cambios

--- Proyecto3/JuegoSuma/main.js
+++ Proyecto3/JuegoSuma/main.js
@@ -1,24 +1,61 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+var num1, num2, respuesta;
+txt_suma = document.getElementById("suma");
+op1 = document.getElementById("op1");
+op2 = document.getElementById("op2");
+op3 = document.getElementById("op3");
+txt_msj = document.getElementById("msj");
+txt_resultado = document.getElementById("resultado");
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+function comenzar(){
+	txt_resultado.innerHTML = "?";
+	txt_msj.innerHTML = "";
 
-setupCounter(document.querySelector('#counter'))
+	//genera la suma - Numeros aleatorios entre 0 1 9
+	num1 = Math.round(Math.random()*9);
+	num2 = Math.round(Math.random()*9);
+	respuesta = num1 + num2;
+	//asignamos lo números para que se muestren
+	suma.innerHTML = num1 + " + " + num2 + " = ";
+
+	//genero un número entre 0 y 2 para colocar la opcion correcta
+	indiceOpCorrecta = Math.round(Math.random()*2);
+	console.log(indiceOpCorrecta);
+
+	//si indiceCorrrecta es igual 0
+	if(indiceOpCorrecta == 0){
+		op1.innerHTML = respuesta;
+		op2.innerHTML = respuesta + 1;
+		op3.innerHTML = respuesta - 1
+	}
+	if(indiceOpCorrecta == 1){
+		op1.innerHTML = respuesta-1;
+		op2.innerHTML = respuesta;
+		op3.innerHTML = respuesta - 2;
+	}
+	if(indiceOpCorrecta == 2){
+		op1.innerHTML = respuesta+ 2;
+		op2.innerHTML = respuesta + 3;
+		op3.innerHTML = respuesta;
+	}
+}
+
+function controlarRespuesta(opcionElegida){	
+
+	txt_resultado.innerHTML = opcionElegida.innerHTML;
+	if(respuesta == opcionElegida.innerHTML){
+		txt_msj.innerHTML = "EXCELENTE!!";
+		txt_msj.style.color="green";
+		setTimeout(comenzar, 2000);
+	}else{
+		txt_msj.innerHTML = "INTENTA DE NUEVO!!";
+		txt_msj.style.color="red";
+		setTimeout(limpiar, 2000);
+
+	}
+}
+function limpiar(){
+	txt_resultado.innerHTML = "?";
+	txt_msj.innerHTML = "";
+}
+
+comenzar();
